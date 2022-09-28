33e9b49932cd506b4da60e0014f6d79c17e63190
Vishal Raj <vishal.kumar4856@gmail.com>
2022-09-29 00:00:57 +0530

    new file

---
+++ ndtv_repl/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Navbar from './Components/Navbar';
+import Opinion from './Components/Opinion';
+import Top from './Components/Top';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      <Top/>
+      <Opinion/>
+    </div>
+  );
+}
+
+export default App;
---
+++ ndtv_repl/src/Components/Top.jsx
@@ -0,0 +1,91 @@
+import React from 'react'
+
+const Top = () => {
+  return (
+    <div style={{display:'flex',gap:"30px",width:"90%",margin:"auto",marginTop:"50px"}} >
+        <div style={{display:"grid",rowGap:"50px",width:"30%"}}>
+            <div style={{backgroundColor:"white",borderRadius:"5px",padding:"1%"}}>
+                <h1>Big Story</h1>
+                <img style={{width:"100%"}} src="https://c.ndtvimg.com/2021-02/hd824m9o_money-generic-rupees-generic_625x300_25_February_21.jpg?downsize=340:209" alt="" />
+                <h2>Dearness Allowance Hiked By 4% For Central Employees, Pensioners: Sources</h2>
+            </div>
+            <div style={{backgroundColor:"white",borderRadius:"5px",padding:"1%"}} >
+                <h2>Top Stories</h2>
+                <div style={{display:'flex',gap:"15px",margin:"10px"}}>
+                    <img src="https://c.ndtvimg.com/2022-09/t8kbhmkg_ankita-bhandari-murder-resort-owner-pulkit-arya-employees-ankit-gupta-and-saurabh-bhaskar-in-police-custody_120x90_28_September_22.jpg?downsize=105:79" alt="" />
+                    <h4>Uttarakhand Teen Murder Case To Be Fast-tracked, ₹25 Lakh Compensation	Uttarakhand Teen Murder Case To Be Fast-tracked, ₹25 Lakh Compensation</h4>
+                </div>
+                <div style={{display:'flex',gap:"15px",margin:"10px"}}>
+                    <img src="https://c.ndtvimg.com/2022-09/q7jki8_diamond-businessman_120x90_28_September_22.jpg?downsize=105:79" alt="" />
+                    <h4>Gujarat Diamond Merchant, Who Barred Staff From Promoting AAP, Joins BJP</h4>
+                </div>
+                <div style={{display:'flex',gap:"15px",margin:"10px"}}>
+                    <img src="https://c.ndtvimg.com/2022-07/m09el48o_hyundai-logo_625x300_13_July_22.jpg?downsize=105:79" alt="" />
+                    <h4>Opinion: Oops, Wrong Hyundai. India's Industrial Policy Misfires - Again</h4>
+                </div>
+                <div style={{display:'flex',gap:"15px",margin:"10px"}}>
+                    <img src="https://c.ndtvimg.com/2022-09/sftpia0g_hilaree-nelson-_120x90_27_September_22.jpg?downsize=105:79" alt="" />
+                    <h4>Missing US Mountaineer's Body Found In Nepal: Expedition Organiser</h4>
+                </div>
+                <div style={{display:'flex',gap:"15px",margin:"10px"}}>
+                    <img src="https://i.ndtvimg.com/i/2017-05/china-police-reuters_240x180_81495791989.jpg?downsize=105:79" alt="" />
+                    <h4>China Opens illegal Police Stations Across Globe:Report</h4>
+                </div>
+                <div style={{display:'flex',gap:"15px",margin:"10px"}}>
+                    <img src="https://c.ndtvimg.com/2022-09/el0k6dv_naseem-shah_625x300_02_September_22.jpg?downsize=105:79" alt="" />
+                    <h4>Pakistan Pacer Naseem Shah Rushed To Hospital With Viral Infection</h4>
+                </div>
+                <div style={{display:'flex',gap:"15px",margin:"10px"}}>
+                    <img src="https://c.ndtvimg.com/javed-miandad_625x300_1526894281860.jpg?downsize=105:79" alt="" />
+                    <h4>"Apni Ghar Ki Team Nahi Hai": Pakistan Great Urges Team To Step Up</h4>
+                </div>
+                <div style={{display:'flex',gap:"15px",margin:"10px"}}>
+                    <img src="https://c.ndtvimg.com/2022-09/cvt00k64_home-ministry-bans-popular-front-of-india-centre-bans-pfi-ban_120x90_28_September_22.jpg?downsize=105:79" alt="" />
+                    <h4>"Ban RSS Too," Says Kerala Opposition, Welcomes Move Against Popular Front</h4>
+                </div>
+            </div>
+        </div>
+        <div>
+            <div style={{backgroundColor:"white",borderRadius:"5px",padding:"3%",marginBottom:"50px"}}>
+                <img width="100%" src="https://c.ndtvimg.com/2022-09/0nakfnjo_ashok-gehlot-pti_625x300_03_September_22.jpg?downsize=570:351" alt="" />
+                <h2 style={{textAlign:"center"}}>"Ashok Gehlot Won't Resign," Says Key Aide Ahead Of Sonia Gandhi Meet</h2>
+            </div>
+            <div style={{backgroundColor:"white",borderRadius:"5px",padding:"3%"}}  >
+                <div style={{display:"flex",gap:"20px"}} >
+                    <h3>Xi's Back, Claims Chinese Media. Here's Why His Appearance Is Big News</h3>
+                    <img src="https://c.ndtvimg.com/2022-09/r201j9q8_xi-jinping_120x90_27_September_22.jpg?downsize=100:75" alt="" />
+                </div>
+                <hr />
+                <div style={{display:"flex",gap:"30px"}} >
+                    <h3>Popular Front Of India Banned For 5 Years Over "Terror Links": 10 Points</h3>
+                    <img src="https://c.ndtvimg.com/2022-09/9g0jr88o_pfi-raids_120x90_28_September_22.jpg?downsize=100:75" alt="" />
+                </div>
+                <hr />
+                <div style={{display:"flex",gap:"30px"}} >
+                    <h3>"Want To Focus On Madhya Pradesh": Kamal Nath On Congress Top Job</h3>
+                    <img src="https://c.ndtvimg.com/2022-09/kip0l898_kamal-nath_120x90_28_September_22.jpg?downsize=100:75" alt="" />
+                </div>
+            </div>
+            <div style={{backgroundColor:"white",borderRadius:"5px",padding:"3%",marginTop:"50px"}}>
+                <div style={{display:"flex",gap:"30px"}}>
+                    <h3>Watch: Astronaut Does Yoga In Space, Perfectly Aces Difficult Pose</h3>
+                    <img src="https://c.ndtvimg.com/2022-09/pvj97kj_ms-cristoforetti-can-be-seen-striking-yoga-poses_625x300_28_September_22.jpg?im=FaceCrop,algorithm=dnn,width=100,height=75" alt="" />
+                </div>
+                <div><hr /></div>
+                <div style={{display:"flex",gap:"30px"}}>
+                    <h3>More Pics From Chum Darang's Arunachal Diaries</h3>
+                    <img src="https://c.ndtvimg.com/2022-09/tk2cngi_chum_120x90_28_September_22.jpg?downsize=100:75" alt="" />
+                </div>
+                <div><hr /></div>
+                <div style={{display:"flex",gap:"30px"}}>
+                    <h3>Mahesh Babu, Namrata Shirodkar's Emotional Posts For Mom Indira Devi</h3>
+                    <img src="https://c.ndtvimg.com/2022-09/4gfa9as_mahesh_120x90_28_September_22.jpg?downsize=100:75" alt="" />
+                </div>
+            </div>
+        </div>
+        <div></div>
+    </div>
+  )
+}
+
+export default Top
